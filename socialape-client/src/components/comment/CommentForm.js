8d004ad5beaa6073d1c06d166947054af01fa939
hungdinh99 <dinhsyhung99@gmail.com>
2021-01-28 20:41:18 +0700

    :wheelchair: Sửa cho chạy được

--- socialape-client/src/components/comment/CommentForm.js
+++ socialape-client/src/components/comment/CommentForm.js
@@ -8,72 +8,77 @@ import { connect } from "react-redux";
 import Localization from "../../localization";
 import { submitComment } from "../../redux/actions/dataAction";
 
-const styles = theme => ({
-  ...theme.spreadThis
+const styles = (theme) => ({
+    ...theme.spreadThis,
 });
 
 export class CommentForm extends Component {
-  state = {
-    body: "",
-    errors: {}
-  };
+    state = {
+        body: "",
+        errors: {},
+    };
 
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.submitComment(this.props.screamId, { body: this.state.body });
-  };
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.submitComment(this.props.screamId, {
+            body: this.state.body,
+        });
+        this.setState({
+            body: "",
+        });
+    };
 
-  render() {
-    const { classes, authenticated } = this.props;
-    const errors = this.state.errors;
-    const commentFormMarkup = authenticated ? (
-      <Grid item sm={12} style={{ textAlign: "center" }}>
-        <form onSubmit={this.handleSubmit}>
-          <TextField
-            name="body"
-            type="text"
-            label={Localization.common.commentPost}
-            error={errors.comment ? true : false}
-            helperText={errors.comment}
-            value={this.state.body}
-            onChange={this.handleChange}
-            fullWidth
-            className={classes.textField}
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-			className={classes.submitButton}
-			disableElevation
-          >
-            {Localization.common.comment}
-          </Button>
-        </form>
-      </Grid>
-    ) : null;
-    return commentFormMarkup;
-  }
+    render() {
+        const { classes, authenticated } = this.props;
+        const errors = this.state.errors;
+        const commentFormMarkup = authenticated ? (
+            <Grid item sm={12} style={{ textAlign: "center" }}>
+                <form onSubmit={this.handleSubmit}>
+                    <TextField
+                        name="body"
+                        type="text"
+                        label={Localization.common.commentPost}
+                        error={errors.comment ? true : false}
+                        helperText={errors.comment}
+                        value={this.state.body}
+                        onChange={this.handleChange}
+                        fullWidth
+                        className={classes.textField}
+                    />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        className={classes.submitButton}
+                        disableElevation
+                    >
+                        {Localization.common.comment}
+                    </Button>
+                </form>
+            </Grid>
+        ) : null;
+        return commentFormMarkup;
+    }
 }
 
 CommentForm.propTypes = {
-  submitComment: PropTypes.func.isRequired,
-  classes: PropTypes.object.isRequired,
-  UI: PropTypes.object.isRequired,
-  authenticated: PropTypes.bool.isRequired
+    submitComment: PropTypes.func.isRequired,
+    classes: PropTypes.object.isRequired,
+    UI: PropTypes.object.isRequired,
+    authenticated: PropTypes.bool.isRequired,
 };
 
-const mapStateToProps = state => ({
-  UI: state.UI,
-  authenticated: state.user.authenticated
+const mapStateToProps = (state) => ({
+    UI: state.UI,
+    authenticated: state.user.authenticated,
 });
 
 export default connect(mapStateToProps, { submitComment })(
-  withStyles(styles)(CommentForm)
+    withStyles(styles)(CommentForm)
 );
